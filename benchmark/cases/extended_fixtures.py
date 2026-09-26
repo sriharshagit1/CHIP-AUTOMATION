567f8f8b6b58da90bc71514371ee7d1757652f44
sriharshagit1 <121559551+sriharshagit1@users.noreply.github.com>
2026-09-26 08:57:41 +0530

    Register extended benchmark cases

---
+++ benchmark/cases/extended_fixtures.py
@@ -0,0 +1,7 @@
+CASES=[
+ ('ASSERTION-001','assertion_bug.sv','assertion_bug_tb.sv'),
+ ('LATCH-001','latch_bug.sv','latch_bug_tb.sv'),
+ ('MUX-001','mux_bug.sv','mux_bug_tb.sv'),
+ ('PARAM-001','parameter_bug.sv','parameter_bug_tb.sv'),
+ ('CDC-001','cdc_bug.sv','cdc_bug_tb.sv'),
+]
